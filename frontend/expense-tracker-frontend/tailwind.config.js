268b1b8d590d06f4f33570be300cf8bb8b12b530
idkwtp-1 <157178374+idkwtp-1@users.noreply.github.com>
2025-05-06 15:23:32 -0400

    tailwind adjisttments

--- frontend/expense-tracker-frontend/tailwind.config.js
+++ frontend/expense-tracker-frontend/tailwind.config.js
@@ -1,1 +1,11 @@
-content: ["./src/**/*.{js,jsx,ts,tsx}"]
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
